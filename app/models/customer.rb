b037e508f563d918ee4d6bcd12610a46ba929aa9
github username <github email address>
2019-03-01 10:37:44 -0600

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,22 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
+
+    def self.all
+      @@all
+    end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def find_by_name(name)
+      full_name
+end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,17 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_by_name(name)
+    Restaurant.first {|restaurant| restaurant.name == self}
   end
 
 end
