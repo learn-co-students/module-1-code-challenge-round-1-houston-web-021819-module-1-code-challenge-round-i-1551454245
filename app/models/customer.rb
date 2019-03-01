cc89c6780372553ef5d0380a9985db5f51668913
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-03-01 10:37:20 -0600

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,27 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
-  def initialize(first_name, last_name)
+  def initialize(first_name: , last_name:)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(full_name)
+    fullName=Customer.all.select do |name|
+      Customer.name == self
+    end
+    fullName.find do |name|
+      fullName.name == self
+    end
+  end
+    
 end
