c71ef79ab30ea955cc0dbf23dc06b21b2e1b171e
Theodore Kimana <kimana.theodore@gmail.com>
2019-03-01 10:42:10 -0600

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,12 @@
 class Restaurant
   attr_accessor :name
-
+@@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
+  def self.all
+    @@all
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,22 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+theodore = Customer.new("Theodore","Kimana")
+james = Customer.new("James","Robert")
+greg = Customer.new("Greg","Jean")
+
+C1= Review.new(theodore,"macdonald","Good",5,)
+C2 = Review.new(james,"bergerking","Awesome",6)
+
+
+
+macdonald = Restaurant.new("Macdonald")
+bergerking = Restaurant.new('Bergerking')
+
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
