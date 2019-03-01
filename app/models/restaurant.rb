874c249ba3c3004f04fc74aa2db955465ad34e62
Thomas_Hansen <ctrhansen@yahoo.com>
2019-03-01 10:36:20 -0600

    Finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,30 @@
 class Restaurant
+  
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def find_by_name
+    Restaurant.all.find do | restaurant_name |
+      restaurant_name.find_by_name == self
+    end
+    name
+  end
+
+
 end
+
+
+# - `Restaurant.all`
+#   - returns an array of all restaurants
+# - `Restaurant.find_by_name(name)`
+#   - given a string of restaurant name, returns the first restaurant that matches
