c3ae51776ce412cdd1e25e72c25b77c408048895
moranja <jeffadammoran@gmail.com>
2019-03-01 10:17:27 -0600

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
-end
+  @@all = []
+  attr_accessor :rating, :content
+  attr_reader :customer, :restaurant
 
+  def initialize (customer:, restaurant:, rating:, content:)
+    @customer = customer
+    @restaurant = restaurant
+    self.rating = rating
+    self.content = content
+    @@all << self
+  end #make rating have to be an integer?
+
+  def self.all
+    @@all
+  end
+end
