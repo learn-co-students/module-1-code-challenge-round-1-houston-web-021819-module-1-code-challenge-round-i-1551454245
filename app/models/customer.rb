909ab5317c06ff2e30baa628dfd3e6367fd5851e
github username <github email address>
2019-03-01 10:32:33 -0600

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,63 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(name) #grab the full name first
+    @@all.find do |customer|
+      customer.full_name == name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.select do |customer|
+      customer.first_name ==  name
+    end
+  end
+
+  def self.all_names
+    @@all.map do |customer|
+      customer.full_name       #use the method bult above
+    end
+  end
+
+  def add_review(restaurant, content, rating) #resturant should pass an instance
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    Review.all.select do |review|
+      review.customer == self
+    end.length
+  end
+
+  def restaurants
+    results = Review.all.select do |review|   
+      review.customer == self
+    end
+    
+    #rest = []
+    #this_rest = nil
+    #results.map do |review|
+     # if this_rest.restaurant != review.restaurant
+      #  rest << review
+      # end
+    #end
+    #rest
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,44 @@
 class Restaurant
   attr_accessor :name
-
+ 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |res|
+      res.name == name
+    end
+  end
+
+  def customers
+   results = Review.all.select do |review|
+      review.restaurant == self
+    end
+   results.map do |result|
+    result.customer
+   end.uniq 
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    total = 0
+    self.reviews.map do |review|
+      total += review.rating
+    end
+    all = self.reviews
+    avg = total/ all.length
   end
 
 end
