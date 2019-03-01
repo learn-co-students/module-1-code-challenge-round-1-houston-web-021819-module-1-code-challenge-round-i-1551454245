490a6ca73b791461a28901fd2e9cd500c8bcf88c
JingChen <jingchenjc2019@gmail.com>
2019-03-01 10:35:35 -0600

    DONE

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
+  @@all = []
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content
+
+  def initialize(customer:, restaurant:, rating:, content:)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    #integer from  1 to 5, to do
+    @content = content
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
   
+  def rating
+    if @rating < 1
+        return nil
+    elsif @rating > 5
+        return nil
+    end
+    return @rating
+  end
+
+  def content
+    @content.to_s
+  end
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,31 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new('a', 'b')
+c2 = Customer.new('c', 'd')
+c3 = Customer.new('a', 'e')
+c1.full_name
+Customer.all
+Customer.find_by_name('c d') #should c2
+Customer.find_all_by_first_name('a') #should [c1,c2]
+Customer.all_names #should['a b','c d','a e']
+
+r1 = Restaurant.new('r1')
+r2 = Restaurant.new('r2')
+
+review1 = Review.new(customer:'a b', restaurant:'r1', rating: 1, content: "Good")
+review2 = Review.new(customer:'c d', restaurant:'r1', rating: 4, content: "Great great")
+
+review3 = Review.new(customer:'c d', restaurant:'r2', rating: 3, content: "Just fine")
+review4 =c1.add_review("r2", 'bad', 1) #given a restaurant object, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+c1.num_reviews #should 2 DONE
+c1.restaurants #should [r2] DONE
+r2.customers #should ['a b', 'c d']
+r1.reviews #should [review1, review2] DONE
+
+
+r1.average_star_rating #should 2.5
+r1.longest_review #should "Great Great"
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
