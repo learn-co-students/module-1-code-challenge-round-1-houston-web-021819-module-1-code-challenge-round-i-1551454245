ff8bd3a5351de39daf4c200e8baa29fdb547cae9
github username <github email address>
2019-03-01 10:57:41 -0600

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,20 @@
 class Restaurant
   attr_accessor :name
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(x)
+    Restaurant.all.select do |rest|
+      rest.name == x
+    end
+  end
 end
